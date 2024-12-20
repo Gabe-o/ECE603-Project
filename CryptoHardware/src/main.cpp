ee1806dad64215e74110bd9f323f93c5e92d5769
fridgetheorem <cameron.hadfield@gmail.com>
2024-12-20 14:12:45 -0500

    Uploaded more data and made modifications to the hardware crypto device

--- CryptoHardware/src/main.cpp
+++ CryptoHardware/src/main.cpp
@@ -8,7 +8,8 @@ char buffer[BUFFER_SIZE];
 int bufferIndex = 0;
 
 byte key[32] = {0}; // 32-byte key for AES-256
-bool useAES = true; // Flag to determine encryption algorithm
+byte deskey[8] = {0}; // 8-byte key for DES
+bool useAES = false; // Flag to determine encryption algorithm
 
 AES256 aes256;
 GCM<AES256> gcmAes256;
@@ -21,6 +22,11 @@ void setNewKey(char* key);
 void printKey();
 
 void setup() {
+  // from https://www.circuitbasics.com/how-to-set-up-uart-communication-for-arduino/
+  pinMode(8, INPUT_PULLUP); // set push button pin as input
+  pinMode(13, OUTPUT);      // set LED pin as output
+  digitalWrite(13, LOW);    // switch off LED pin
+
   Serial.begin(9600);
   while (!Serial) {
     ; // Wait for serial port to connect
@@ -50,9 +56,9 @@ void processCommand() {
   } else if (strcmp(buffer, "ALG AES") == 0) {
     useAES = true;
     Serial.println("Using AES encryption");
-  } else if (strcmp(buffer, "ALG CHACHA") == 0) {
+  } else if (strcmp(buffer, "ALG DES") == 0) {
     useAES = false;
-    Serial.println("Using ChaCha encryption");
+    Serial.println("Using DES encryption");
   } else if (strcmp(buffer, "GETKEY") == 0) {
     printKey();
   } else {
@@ -72,12 +78,12 @@ void encryptMessage(char* message) {
     
     // Serial.print("Encrypted (AES): ");
   } else {
+    des.set_IV(0);
+    des.encrypt(ciphertext, message, deskey);
     // byte nonce[12] = {0}; // Nonce for ChaCha
     // chacha.setKey(key, 32);
     // chacha.setIV(nonce, 12);
     // chacha.encrypt(ciphertext, (byte*)message, messageLen);
-    
-    // Serial.print("Encrypted (ChaCha): ");
   }
   
   for (int i = 0; i < messageLen; i++) {
@@ -87,10 +93,18 @@ void encryptMessage(char* message) {
 }
 
 void setNewKey(char* newKey) {
-  int keyLen = strlen(newKey);
-  if (keyLen > 32) keyLen = 32;
-  memset(key, 0, 32);
-  memcpy(key, newKey, keyLen);
+  if (useAES){
+    int keyLen = strlen(newKey);
+    if (keyLen > 32) keyLen = 32;
+    memset(key, 0, 32);
+    memcpy(key, newKey, keyLen);
+  }
+  else{
+    int keyLen = strlen(newKey);
+    if (keyLen > 8) keyLen = 8;
+    memset(deskey, 0, 8); // Set the key for DES
+    memcpy(deskey, newKey, keyLen);
+  }
   Serial.println("New key set");
 }
 
